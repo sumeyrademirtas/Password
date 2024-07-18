6accc19dc01fbd3a2fe3f15c2d1069b35c0a18dd
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-07-18 14:52:03 +0300

    feat(ui): add password criteria views and message to PasswordStatusView
    # added criteriaLabel to PasswordStatusView for displaying criteria message.
    # created instances of PasswordCriteriaView for different password criteria.
    # set stackView distribution to .equalCentering.
    # added makeCriteriaMessage method to generate attributed string for criteriaLabel.
    # set translatesAutoresizingMaskIntoConstraints to false for all criteria views.

--- Password/PasswordStatusView.swift
+++ Password/PasswordStatusView.swift
@@ -11,6 +11,15 @@ import UIKit
 class PasswordStatusView: UIView {
     
     let stackView = UIStackView()
+    let criteriaLabel = UILabel()
+   
+    let lengthCriteriaView = PasswordCriteriaView(text: "8-32 characters (no spaces)")
+    let upperCaseCriteriaView = PasswordCriteriaView(text: "uppercase letter (A-Z)")
+    let lowerCaseCriteriaView = PasswordCriteriaView(text: "lowercase (a-z)")
+    let digitCriteriaView = PasswordCriteriaView(text: "digit (0-9)")
+    let specialCharacterCriteriaView = PasswordCriteriaView(text: "special character (e.g. !@#$%ˆ)")
+    
+
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -37,10 +46,31 @@ extension PasswordStatusView {
         stackView.axis = .vertical
         stackView.spacing = 8
         stackView.backgroundColor = .systemRed
+        stackView.distribution = .equalCentering
+        
+        criteriaLabel.translatesAutoresizingMaskIntoConstraints = false
+        criteriaLabel.numberOfLines = 0
+        criteriaLabel.lineBreakMode = .byWordWrapping
+        criteriaLabel.attributedText = makeCriteriaMessage()
+        
+        lengthCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        upperCaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        lowerCaseCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        digitCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        specialCharacterCriteriaView.translatesAutoresizingMaskIntoConstraints = false
+        
+      
         
     }
     
     func layout() {
+        stackView.addArrangedSubview(lengthCriteriaView)
+        stackView.addArrangedSubview(criteriaLabel)
+        stackView.addArrangedSubview(upperCaseCriteriaView)
+        stackView.addArrangedSubview(lowerCaseCriteriaView)
+        stackView.addArrangedSubview(digitCriteriaView)
+        stackView.addArrangedSubview(specialCharacterCriteriaView)
+        
         addSubview(stackView)
         
         NSLayoutConstraint.activate([
@@ -51,4 +81,22 @@ extension PasswordStatusView {
         ])
         
     }
+    
+    private func makeCriteriaMessage() -> NSAttributedString {
+        var plainTextAttributes = [NSAttributedString.Key: AnyObject]()
+        plainTextAttributes[.font] = UIFont.preferredFont(forTextStyle: .subheadline)
+        plainTextAttributes[.foregroundColor] = UIColor.secondaryLabel
+        
+        var boldTextAttributes = [NSAttributedString.Key: AnyObject]()
+        boldTextAttributes[.foregroundColor] = UIColor.label
+        boldTextAttributes[.font] = UIFont.preferredFont(forTextStyle: .subheadline)
+
+        let attrText = NSMutableAttributedString(string: "Use at least ", attributes: plainTextAttributes)
+        attrText.append(NSAttributedString(string: "3 of these 4 ", attributes: boldTextAttributes))
+        attrText.append(NSAttributedString(string: "criteria when setting your password:", attributes: plainTextAttributes))
+
+        return attrText
+    }
 }
+
+
