ce303b8cb064967247f1d7c465cbb57375686c8b
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-07-18 10:31:32 +0300

    feat(ui): add stack view
    # created stackView in ViewController with vertical axis and spacing.
    # added newPasswordTextField to stackView as an arranged subview.
    # added stackView to the view hierarchy.
    # set constraints for stackView to center it vertically and adjust leading and trailing anchors.

--- Password/ViewController.swift
+++ Password/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let stackView = UIStackView()
     let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
     
     override func viewDidLoad() {
@@ -19,22 +20,24 @@ class ViewController: UIViewController {
 
 extension ViewController {
     func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
         newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
         
     }
     
     func layout() {
-        view.addSubview(newPasswordTextField)
+        stackView.addArrangedSubview(newPasswordTextField)
+        view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            newPasswordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            newPasswordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2)
+            
         ])
         
-        NSLayoutConstraint.activate([
-            newPasswordTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: newPasswordTextField.trailingAnchor, multiplier: 1),
-            newPasswordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
 }
